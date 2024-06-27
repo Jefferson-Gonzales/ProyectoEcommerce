044f59a003116fe1bbb919df743320a127da1552
jefferson <jeffersongonzales@gmail.com>
2024-06-27 16:04:40 -0500

    Creacion de Clases y Metodos

---
+++ src/main/java/com/proyecto/service/impl/ProductoServiceImpl.java
@@ -0,0 +1,42 @@
+package com.proyecto.service.impl;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.proyecto.model.Producto;
+import com.proyecto.repository.ProductoRepository;
+import com.proyecto.service.ProductoService;
+
+@Service
+public class ProductoServiceImpl implements ProductoService{
+
+	@Autowired
+	private ProductoRepository productoRepository;
+	
+	//Metodo para guardar datos
+	@Override
+	public Producto save(Producto producto) {
+		return productoRepository.save(producto);
+	}
+
+	//Metodo para obtener datos
+	@Override
+	public Optional<Producto> get(Integer id) {
+		return productoRepository.findById(id);
+	}
+
+	//Metodo para actualizar datos
+	@Override
+	public void update(Producto producto) {
+		productoRepository.save(producto);
+	}
+
+	//Metodo para eliminar datos
+	@Override
+	public void delete(Integer id) {
+		productoRepository.deleteById(id);
+	}
+
+}
