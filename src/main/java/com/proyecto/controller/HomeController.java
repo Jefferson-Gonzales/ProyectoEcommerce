aef91f0a06e4271b25ab99fceab746ae2c2fe943
Jefferson Gonzales <jeffersongonzales@gmail.com>
2024-07-11 19:59:12 -0500

    Implementacion de la vista producto home

--- src/main/java/com/proyecto/controller/HomeController.java
+++ src/main/java/com/proyecto/controller/HomeController.java
@@ -9,7 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.proyecto.model.Producto;
 import com.proyecto.service.ProductoService;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/")
@@ -29,8 +32,13 @@ public class HomeController {
 	}
 	
 	@GetMapping("productohome/{id}")
-	public String productoHome(@PathVariable Integer id) {
+	public String productoHome(@PathVariable Integer id, Model model) {
 		log.info("Id producto enviado como parametro {}", id);
+		Producto producto = new Producto();
+		Optional<Producto> productoOptional = productoService.get(id);
+		producto = productoOptional.get();
+
+		model.addAttribute("producto", producto);
 		return "usuario/productohome";
 	}
 }
